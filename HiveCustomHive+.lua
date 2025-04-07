a620b64cb361a6c652ba26a1a38821e8db7a6cbe
CutieNamenu <loadiine.uwu@gmail.com>
2025-04-07 05:47:58 -0700

    Updated the description

--- HiveCustomHive+.lua
+++ HiveCustomHive+.lua
@@ -1,14 +1,17 @@
 
 name = "CustomHivePlus"
-description = "allow you to do .hive+ in the chat (the Nickname module Destroy the script)"
+description = "Allow you to do .hive+"
 
-local file = io.open("hive+clr", "r")
+info = "Known Issues :\nIf you have and Hub Title the script will not work\n\nIf you're using the module nickname disable it before using the command\nYOU CANNOT USE THE SCRIPT AGAIN IF YOU USE NICKNAME (except if you restart Onix)"
+client.settings.addInfo(info)
+
+local file = io.open("hive+.clr", "r")
 
 if file then
     currentColor = file:read("*all")
     file:close()
 else
-    local file = io.open("hive+clr", "w")
+    local file = io.open("hive+.clr", "w")
     file:write("a")
     file:close()
     currentColor = "a"
